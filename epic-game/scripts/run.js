da1d75ec7cc5c5f06ca85ca4385ecd0a011d59d5
Atreay Kukanur <www.atreayk@gmail.com>
2023-03-06 13:07:09 +0530

    King_Slayer

---
+++ epic-game/scripts/run.js
@@ -0,0 +1,47 @@
+const main = async () => {
+    const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
+    const gameContract = await gameContractFactory.deploy(
+      ["Aizan", "Kenny", "Yhwach"],       // Names
+      ["https://imgur.com/Wr2Xq4q.png", // Images
+      "https://imgur.com/LvKJRYB.png", 
+      "https://imgur.com/1BU2T35.png"],
+      [100, 200, 300],                    // HP values
+      [100, 70, 200],                       // Attack damage values
+      "Soul King", // Boss name
+      "https://imgur.com/5shGdeL.png", // Boss image
+      10000, // Boss hp
+      50 // Boss attack damage
+    );
+    await gameContract.deployed();
+    console.log("Contract deployed to:", gameContract.address);
+
+    let txn;
+    // We only have three characters.
+    // an NFT w/ the character at index 2 of our array.
+    txn = await gameContract.mintCharacterNFT(1);
+    await txn.wait();
+
+    txn = await gameContract.attackBoss();
+    await txn.wait();
+
+    txn = await gameContract.attackBoss();
+    await txn.wait();
+
+    // Get the value of the NFT's URI.
+    // let returnedTokenUri = await gameContract.tokenURI(1);
+    // console.log("Token URI:", returnedTokenUri);
+
+    console.log("Done!");
+  };
+  
+  const runMain = async () => {
+    try {
+      await main();
+      process.exit(0);
+    } catch (error) {
+      console.log(error);
+      process.exit(1);
+    }
+  };
+
+  runMain();
